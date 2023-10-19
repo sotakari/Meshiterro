32cc12ed3b8d1630e87458e91ec9af4aa3d8d700
sotakari <146611649+sotakari@users.noreply.github.com>
2023-10-19 09:02:46 +0000

    [Finish] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
     
   devise_for :users
 root to: "homes#top"
-resources :post_images, only: [:new, :index, :show]
+resources :post_images, only: [:new, :create, :index, :show]
 get "homes/about" => "homes#about", as: "about"
 end
