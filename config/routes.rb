dc67e5b8597e14c73cbe2f931b98af37ea246988
sotakari <146611649+sotakari@users.noreply.github.com>
2023-10-22 14:15:04 +0000

    [Finish] 4-16

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ devise_for :users
 root to: "homes#top"
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
 get "homes/about" => "homes#about", as: "about"
-resources :users, only:[:show, :edit]
+resources :users, only:[:show, :edit, :update]
 end
